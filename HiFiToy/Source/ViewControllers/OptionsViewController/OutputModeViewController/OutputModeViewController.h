00435fd0a1539c078ddedbc4e2fa082ca9a6bd0e
kerosinn <trimp@yandex.ru>
2021-05-08 16:48:29 +0300

    added Output mode view controller

---
+++ HiFiToy/Source/ViewControllers/OptionsViewController/OutputModeViewController/OutputModeViewController.h
@@ -0,0 +1,17 @@
+//
+//  OutputModeViewController.h
+//  HiFiToy
+//
+//  Created by Kerosinn_OSX on 07/05/2021.
+//  Copyright © 2021 Kerosinn_OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OutputModeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HiFiToy/Source/ViewControllers/OptionsViewController/OutputModeViewController/OutputModeViewController.m
@@ -0,0 +1,32 @@
+//
+//  OutputModeViewController.m
+//  HiFiToy
+//
+//  Created by Kerosinn_OSX on 07/05/2021.
+//  Copyright © 2021 Kerosinn_OSX. All rights reserved.
+//
+
+#import "OutputModeViewController.h"
+
+@interface OutputModeViewController ()
+
+@end
+
+@implementation OutputModeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
